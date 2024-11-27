7329748484b841cb4fcf0608822d5b4f285ef82d
AMS-JR <asj.sarjo@gmail.com>
2024-11-27 03:52:29 -0600

    Initial commit of inClassDemo

---
+++ src/main/java/inClassDemo/Main.java
@@ -0,0 +1,57 @@
+package inClassDemo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int n = 5;
+//            for (int i = 0; i < n; ++i) {
+//                for (int j = 0; j < n; ++j) {
+//                    System.out.printf("%-3s", "*");
+//                }
+//                System.out.println();
+//            }
+//
+//        for(int i = 0; i < n; ++i){
+//            for(int j = 0; j <= i; ++j){
+//                System.out.printf("%-3s","*");
+//            }
+//            System.out.println();
+//        }
+        // Using Strings
+//        int len = 0;
+//        if(args != null) len = args.length;
+//        for(int i = 0; i < len; ++i) {
+//            System.out.println("position " + i + ": " + args[i]);
+//        }
+//
+//        if(args == null || args.length == 0) {
+//            System.out.println("<no input>");
+//        }
+//        String finalSentence = "";
+//        len = args.length;
+//        for(int i = 0; i < len-1; ++i) {
+//            finalSentence += (args[i] + " "); //inefficient
+//        }
+//        finalSentence += (args[len-1] + ".");
+//        System.out.println(finalSentence);
+
+        //Using StringBuilder
+        int len = 0;
+        if(args == null || args.length == 0) {
+            System.out.println("<no input>");
+        }
+        StringBuilder finalSentence = new StringBuilder();
+        len = args.length;
+        for(int i = 0; i < len-1; ++i) {
+            finalSentence.append(args[i]);
+            finalSentence.append(" "); //much more efficient
+        }
+        finalSentence.append(args[len-1]);
+        finalSentence.append(".");
+// Convert the StringBuilder to a String at the end.
+        String finalSentenceAsString = finalSentence.toString();
+        System.out.println(finalSentenceAsString);
+
+    }
+}
